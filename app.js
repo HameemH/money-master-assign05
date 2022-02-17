74306552d3688dd4a0628c27ca87166bbb4c6f81
HameemH <hameemhossain2@gmail.com>
2022-02-17 11:53:19 +0600

    all calc done

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 function inputValue(inputId){
     const amountText = document.getElementById(inputId).value;
     const amount = parseFloat(amountText);
+    
+    
     return amount;
+    
 }
 
 document.getElementById('calculate').addEventListener('click', function(){
@@ -10,16 +13,39 @@ document.getElementById('calculate').addEventListener('click', function(){
  const foodExpense = inputValue('food-amount');
  const rentExpense = inputValue('rent-amount');
  const clothExpense = inputValue('cloth-amount')
- const totalExpenseText = document.getElementById('total-expense');
- const totalExpenseAmount = parseFloat(totalExpenseText);
- const newTotalExpenseAmont = foodExpense+ rentExpense +clothExpense;
- totalExpenseText.innerText = newTotalExpenseAmont;
-
- const remainingBalanceText = document.getElementById('remaining-balance');
- const remainingBalanceAmount = parseFloat(remainingBalanceText);
- const RemainingBalance = incomeAmount - newTotalExpenseAmont;
+ if(incomeAmount < 0 || foodExpense < 0 || rentExpense < 0 || clothExpense < 0){
+     alert('please put a positive number');
+    
+ }
  
- remainingBalanceText.innerText = RemainingBalance;
+ else{
+    const totalExpenseText = document.getElementById('total-expense');
+    const totalExpenseAmount = parseFloat(totalExpenseText);
+    const newTotalExpenseAmont = foodExpense+ rentExpense +clothExpense;
+    totalExpenseText.innerText = newTotalExpenseAmont;
+   
+    const remainingBalanceText = document.getElementById('remaining-balance');
+    const remainingBalanceAmount = parseFloat(remainingBalanceText);
+    const RemainingBalance = incomeAmount - newTotalExpenseAmont;
+    
+    remainingBalanceText.innerText = RemainingBalance;
 
+ }
+})
+
+document.getElementById('savings').addEventListener('click', function(){
+    const incomeAmount = inputValue('income-amount');
+    const savingPercentage = inputValue('saving-percent');
+    const savingAmountText = document.getElementById('saving-amount');
+    const savingInitial = savingPercentage / 100;
+    const savingAmount = incomeAmount * savingInitial;
+    savingAmountText.innerText = savingAmount;
+
+
+     const remainingBalAfterSaving = document.getElementById('amount-after-saving');
+     const remainingBalanceText = document.getElementById('remaining-balance').innerText;
+    const remainingBalanceAmount = parseFloat(remainingBalanceText);
+     const remainingBalAfterSavingAmount = remainingBalanceAmount - savingAmount;
+     remainingBalAfterSaving.innerText = remainingBalAfterSavingAmount;
 
 })
